3db852118be66659e854d073577c0648ef2d3275
이서율 <yul9910@naver.com>
2024-09-30 13:57:05 +0900

    Initial commit

---
+++ src/main/java/com/spring_boots/spring_boots/SpringBootsApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boots.spring_boots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring_boots/spring_boots/SpringBootsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boots.spring_boots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
