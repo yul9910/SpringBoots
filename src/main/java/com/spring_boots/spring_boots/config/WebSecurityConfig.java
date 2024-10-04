f1bae9ed805f7603136fca747cdbd02367952ab0
이서율 <yul9910@naver.com>
2024-10-04 21:12:06 +0900

    feat: 의존성 추가 및 요청 허가

--- src/main/java/com/spring_boots/spring_boots/config/WebSecurityConfig.java
+++ src/main/java/com/spring_boots/spring_boots/config/WebSecurityConfig.java
@@ -49,7 +49,7 @@ public class WebSecurityConfig {
                         .requestMatchers(
                                 "/api/**","/login/**"
                         ).permitAll()  // 모든 요청에 대해 요청 허가
-                        .anyRequest().authenticated()) //모든 요청에 대해 인증
+                        .anyRequest().permitAll())
 
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
