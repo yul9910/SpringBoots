5baee908eb2ddce3cca0906898603228ad08fa9b
[CLOUD4] 고범석 <kbssky94@gmail.com>
2024-10-04 11:37:44 +0900

    comment: EventApiController 주석과 카테고리 Mapper 접근지정자 수정

--- src/main/java/com/spring_boots/spring_boots/category/dto/CategoryMapper.java
+++ src/main/java/com/spring_boots/spring_boots/category/dto/CategoryMapper.java
@@ -8,24 +8,24 @@ import org.mapstruct.MappingTarget;
 
 
 @Mapper(componentModel = "spring")
-public abstract class CategoryMapper {
+public interface CategoryMapper {
 
   // Category -> CategoryResponseDto
-  public abstract CategoryResponseDto categoryToCategoryResponseDto(Category category);
+  CategoryResponseDto categoryToCategoryResponseDto(Category category);
 
   // Category -> CategoryDto
-  public abstract CategoryDto categoryToCategoryDto(Category category);
+  CategoryDto categoryToCategoryDto(Category category);
 
   // Category -> CategoryAdminDto
-  public abstract CategoryAdminDto categoryToCategoryAdminDto(Category category);
+  CategoryAdminDto categoryToCategoryAdminDto(Category category);
 
   // CategoryRequestDto -> Category
   @Mapping(target = "id", ignore = true)
-  public abstract Category categoryRequestDtoToCategory(CategoryRequestDto requestDto);
+  Category categoryRequestDtoToCategory(CategoryRequestDto requestDto);
 
   // CategoryRequestDto -> Category (업데이트)
   @Mapping(target = "id", ignore = true)
-  public abstract void updateCategoryFromDto(CategoryRequestDto dto, @MappingTarget Category category);
+  void updateCategoryFromDto(CategoryRequestDto dto, @MappingTarget Category category);
 
 }
 
