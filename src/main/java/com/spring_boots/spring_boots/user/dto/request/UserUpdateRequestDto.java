35598f44b9c935d62d82da6e22d4cb382bf52ab6
[CLOUD4] 차현승 <gustmd9694@gmail.com>
2024-10-08 22:43:51 +0900

    feat: S3Bucket 기능 추가

---
+++ src/main/java/com/spring_boots/spring_boots/user/dto/request/UserUpdateRequestDto.java
@@ -0,0 +1,9 @@
+package com.spring_boots.spring_boots.user.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class UserUpdateRequestDto {
+    private String password;
+    private String email;
+}
