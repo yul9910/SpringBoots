75b4263db80b1e11c9fb0c8e86ee3379643d21eb
[CLOUD4] 차현승 <gustmd9694@gmail.com>
2024-10-08 15:39:54 +0900

    feat: 상품 테스트코드 추가

--- src/test/java/com/spring_boots/spring_boots/item/ItemRestControllerTest.java
+++ src/test/java/com/spring_boots/spring_boots/item/ItemRestControllerTest.java
@@ -1,15 +1,24 @@
 package com.spring_boots.spring_boots.item;
 
+import com.amazonaws.Response;
 import com.spring_boots.spring_boots.item.controller.ItemRestController;
+import com.spring_boots.spring_boots.item.dto.CreateItemDto;
+import com.spring_boots.spring_boots.item.dto.ResponseItemDto;
 import com.spring_boots.spring_boots.item.service.ItemRestService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class ItemRestControllerTest {
 
@@ -27,4 +36,20 @@ public class ItemRestControllerTest {
         MockitoAnnotations.openMocks(this);
         mockMvc = MockMvcBuilders.standaloneSetup(itemRestController).build();
     }
+
+    @Test
+    public void testCreateItem() throws Exception {
+        // 필드 설정
+        CreateItemDto createItemDto = new CreateItemDto();
+        // 필드 설정
+        ResponseItemDto responseItemDto = new ResponseItemDto();
+
+        when(itemRestService.createItem(any(CreateItemDto.class))).thenReturn(responseItemDto);
+
+        mockMvc.perform(post("/api/admin/items")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"field\":\"value\"}"))
+                .andExpect(status().isCreated());
+    }
+    
 }
