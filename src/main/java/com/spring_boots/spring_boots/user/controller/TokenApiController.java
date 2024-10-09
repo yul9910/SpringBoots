6a68481531b6df9319cf9ad75647651020ca90fe
이찬진 <chanjin23@naver.com>
2024-10-09 19:46:20 +0900

    feat : HttpOnly 주석, 로그인 응답데이터 관리자 체크 추가

--- src/main/java/com/spring_boots/spring_boots/user/controller/TokenApiController.java
+++ src/main/java/com/spring_boots/spring_boots/user/controller/TokenApiController.java
@@ -1,5 +1,6 @@
 package com.spring_boots.spring_boots.user.controller;
 
+import com.spring_boots.spring_boots.user.domain.UserRole;
 import com.spring_boots.spring_boots.user.dto.request.JwtTokenDto;
 import com.spring_boots.spring_boots.user.dto.request.JwtTokenLoginRequest;
 import com.spring_boots.spring_boots.user.dto.request.RefreshTokenRequest;
@@ -10,6 +11,7 @@ import com.spring_boots.spring_boots.user.service.UserService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,7 +32,7 @@ public class TokenApiController {
         // 기존 쿠키 삭제 로직
         if (existingRefreshTokenCookie != null) {
             Cookie deleteRefreshTokenCookie = new Cookie("refreshToken", null);
-            deleteRefreshTokenCookie.setHttpOnly(true); // 자바스크립트에서 접근 불가
+//            deleteRefreshTokenCookie.setHttpOnly(true); // 자바스크립트에서 접근 불가
 //            deleteRefreshTokenCookie.setSecure(true); // HTTPS에서만 전송
             deleteRefreshTokenCookie.setPath("/"); // 동일한 경로
             deleteRefreshTokenCookie.setMaxAge(0); // 쿠키 삭제 설정
@@ -45,7 +47,7 @@ public class TokenApiController {
                 jwtTokenResponse.getRefreshToken()
         );
 
-        refreshTokenCookie.setHttpOnly(true); // 자바스크립트에서 접근할 수 없도록 설정
+//        refreshTokenCookie.setHttpOnly(true); // 자바스크립트에서 접근할 수 없도록 설정
 //        refreshTokenCookie.setSecure(true); // HTTPS에서만 전송되도록 설정 (생산 환경에서 사용)
         refreshTokenCookie.setPath("/"); // 쿠키의 유효 경로 설정
         refreshTokenCookie.setMaxAge(7 * 24 * 60 * 60); // 쿠키의 유효 기간 설정 (예: 7일)
@@ -56,6 +58,7 @@ public class TokenApiController {
         return ResponseEntity.ok().body(JwtTokenResponse
                 .builder()
                 .accessToken(jwtTokenResponse.getAccessToken())
+                .isAdmin(jwtTokenResponse.getRole().equals(UserRole.ADMIN))
                 .build());
     }
 
@@ -71,6 +74,15 @@ public class TokenApiController {
         }
 
         return ResponseEntity.ok(new RefreshTokenResponse(newAccessToken));
+    }
+
+    @GetMapping("/api/protected")
+    public ResponseEntity<String> getProtectedResource(@CookieValue("accessToken") String accessToken) {
+        if (userService.validateToken(accessToken)) {
+            return ResponseEntity.ok("Protected data");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+        }
     }
 
 }
