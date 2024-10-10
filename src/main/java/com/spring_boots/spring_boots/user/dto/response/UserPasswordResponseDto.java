11de7722300ad19e156af607dd65ddd58b38baa4
이찬진 <chanjin23@naver.com>
2024-10-11 03:08:04 +0900

    feat : 프론트 회원정보삭제 구현완료

---
+++ src/main/java/com/spring_boots/spring_boots/user/dto/response/UserPasswordResponseDto.java
@@ -0,0 +1,10 @@
+package com.spring_boots.spring_boots.user.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserPasswordResponseDto {
+    private long id;
+}
