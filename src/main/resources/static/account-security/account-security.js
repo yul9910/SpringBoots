5123b327c3d97eee7dd8735abc9f814cbacafd39
이찬진 <chanjin23@naver.com>
2024-10-11 03:41:20 +0900

    feat : 회원정보 관리 임시(팀원 상의 필요)

--- src/main/resources/static/account-security/account-security.js
+++ src/main/resources/static/account-security/account-security.js
@@ -102,7 +102,7 @@ function toggleTargets(e) {
 // 나중에 사용자가 데이터를 변경했는지 확인하기 위해, 전역 변수로 userData 설정
 let userData;
 async function insertUserData() {
-  userData = await Api.get("/users");
+  userData = await Api.get("/api/users-info");
 
   // 객체 destructuring
   const { fullName, email, address, phoneNumber } = userData;
