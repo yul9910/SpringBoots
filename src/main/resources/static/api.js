4fcd19e8f4bc325ed7bf7447f492eba116faee7b
[CLOUD4] 고범석 <kbssky94@gmail.com>
2024-10-15 04:41:11 +0900

    feat: 카테고리 추가 및 수정 구현 + 이미지 삽입기능 + 미리보기

--- src/main/resources/static/api.js
+++ src/main/resources/static/api.js
@@ -29,8 +29,7 @@ async function get(endpoint, params = "") {
 async function post(endpoint, data) {
   const apiUrl = endpoint;
   const bodyData = JSON.stringify(data);
-  console.log(`%cPOST 요청: ${apiUrl}`, "color: #296aba;");
-  console.log(`%cPOST 요청 데이터: ${bodyData}`, "color: #296aba;");
+
 
   // 토큰이 있으면 Authorization 헤더를 포함, 없으면 포함하지 않음
 //  const token = sessionStorage.getItem("token");
@@ -148,6 +147,50 @@ async function del(endpoint, params = "", data = {}) {
   return result;
 }
 
+async function postFormData(endpoint, formData) {
+  const apiUrl = endpoint;
+  console.log(`%cPOST FormData 요청: ${apiUrl}`, "color: #059c4b;");
+
+  try {
+    const res = await fetch(apiUrl, {
+      method: "POST",
+      body: formData,
+      credentials: "include",
+    });
+
+    if (!res.ok) {
+      const errorContent = await res.json();
+      console.error("Error response:", errorContent);
+      throw new Error(errorContent.message || "서버 오류가 발생했습니다.");
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+}
+
+async function patchFormData(endpoint, params = "", formData) {
+  const apiUrl = params ? `${endpoint}/${params}` : endpoint;
+  console.log(`%cPATCH FormData 요청: ${apiUrl}`, "color: #059c4b;");
+
+  const res = await fetch(apiUrl, {
+    method: "PATCH",
+    body: formData,
+    credentials: "include",
+  });
+
+  if (!res.ok) {
+    const errorContent = await res.json();
+    const { reason } = errorContent;
+    throw new Error(reason);
+  }
+
+  const result = await res.json();
+  return result;
+}
+
 
 // 아래처럼 export하면, import * as Api 로 할 시 Api.get, Api.post 등으로 쓸 수 있음.
-export { get, post, patch, del as delete };
+export { get, post, patch, del as delete, postFormData, patchFormData };
