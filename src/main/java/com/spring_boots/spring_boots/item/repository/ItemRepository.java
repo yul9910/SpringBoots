bc8294b059f23fac03f1712990615fdf4047e4f2
[CLOUD4] 고범석 <kbssky94@gmail.com>
2024-10-17 19:55:42 +0900

    feat: 상품 엔티티 키워드 리스트 추가, 관련 메서드와 데이터 추가

--- src/main/java/com/spring_boots/spring_boots/item/repository/ItemRepository.java
+++ src/main/java/com/spring_boots/spring_boots/item/repository/ItemRepository.java
@@ -1,6 +1,8 @@
 package com.spring_boots.spring_boots.item.repository;
 
 import com.spring_boots.spring_boots.item.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,5 +10,6 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long> {
   List<Item> findAllByCategoryId(Long categoryId);
 
-  List<Item> findByKeywordsContainingIgnoreCase(String keyword);
+  // 키워드를 대소문자 구분없이 포함하면 페이지로 검색
+  Page<Item> findByKeywordsContainingIgnoreCase(String keyword, Pageable pageable);
 }
