12c64012ef4e0999ae837426ce2138c2b3980f9d
[CLOUD4] 고범석 <kbssky94@gmail.com>
2024-10-20 17:49:16 +0900

    feat: 이벤트 시작일, 종료일 미기입시 자동 설정

--- src/main/resources/static/event-form/event-form.js
+++ src/main/resources/static/event-form/event-form.js
@@ -85,7 +85,7 @@ function formatDate(date) {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
-    return `${year}.${month}.${day}.`;
+    return `${year}-${month}-${day}`;
 }
 
 // 현재 날짜 구하기
